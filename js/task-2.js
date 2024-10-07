5f15400c0dcd142070f3a2580c0648d891fdc0e2
ruslan-tykhonov <RUSLANEGOROVICH1943@GMAIL.COM>
2024-10-07 19:06:01 +0200

    change

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,14 @@
-function makeArray(firstArray, secondArray, maxLength) {
-  // Створюємо новий масив, об'єднуючи два масиви
-  const combinedArray = firstArray.concat(secondArray);
-
-  // Якщо довжина нового масиву більша за maxLength, повертаємо його копію з maxLength елементів
-  if (combinedArray.length > maxLength) {
-    return combinedArray.slice(0, maxLength);
+function makeArray(firstArray, secondArray, maxLenght) {
+  const resultArray = firstArray.concat(secondArray);
+  if (resultArray.length < maxLenght) {
+    return resultArray;
   }
-
-  // Якщо довжина не перевищує maxLength, повертаємо весь масив
-  return combinedArray;
+  return resultArray.slice(0, maxLenght);
 }
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,15 @@
 function filterArray(numbers, value) {
-  // Створюємо порожній масив для зберігання підходящих чисел
-  const filteredNumbers = [];
-
-  // Проходимо через кожен елемент масиву numbers
-  for (let i = 0; i < numbers.length; i++) {
-    // Якщо число більше за value, додаємо його до нового масиву
-    if (numbers[i] > value) {
-      filteredNumbers.push(numbers[i]);
+  const newArray = [];
+  for (const a of numbers) {
+    if (a > value) {
+      newArray.push(a);
     }
   }
-
-  // Повертаємо новий масив з підходящими числами
-  return filteredNumbers;
+  return newArray;
 }
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
